31d76fa7f5dbd7695a28a837bba531b19c7880d6
Frank <frank.wiebeler@dev4agriculture.de>
2020-04-20 09:13:11 +0200

    -FIX: Adjusted namings TMT to "TechnicalMessageType" as this is better understandable

--- dotnet/generate.sh
+++ dotnet/generate.sh
@@ -4,14 +4,14 @@ rm -rf generated/Agrirouter
 printf 'Create folders.\n\n'
 mkdir generated
 mkdir generated/Agrirouter
-mkdir generated/Agrirouter/TMT
+mkdir generated/Agrirouter/TechnicalMessageType
 
-printf 'Generating files | TMT\n\n' 
-protoc -Igoogle/protobuf -Iagrirouter/protobuf/ --csharp_out=generated/Agrirouter/TMT agrirouter/protobuf/tmt/*.proto
+printf 'Generating files | TechnicalMessageTypes\n\n' 
+protoc -Igoogle/protobuf -Iagrirouter/protobuf --csharp_out=generated/Agrirouter/TechnicalMessageType agrirouter/protobuf/technicalmessagetype/*.proto
 
 printf 'Exchange files from the solution.\n\n'
-rm -rf AgrirouterTMTProtobufDefinitions/AgrirouterTMTProtobufDefinitions/Agrirouter
-mv generated/Agrirouter AgrirouterTMTProtobufDefinitions/AgrirouterTMTProtobufDefinitions
+rm -rf AgrirouterTechnicalMessageTypeProtobufDefinitions/AgrirouterTechnicalMessageTypeProtobufDefinitions/Agrirouter
+mv generated/Agrirouter AgrirouterTechnicalMessageTypeProtobufDefinitions/AgrirouterTechnicalMessageTypeProtobufDefinitions
 
 printf 'Clean up.\n\n'
 rm -rf generated
